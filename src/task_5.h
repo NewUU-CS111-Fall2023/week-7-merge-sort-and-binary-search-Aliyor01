2a5a77c92237f436109e5299a227f95487388934
Aliyor01 <127713784+Aliyor01@users.noreply.github.com>
2023-12-04 22:18:26 +0500

    Update task_5.h

--- src/task_5.h
+++ src/task_5.h
@@ -3,3 +3,57 @@
  * Date: 04.12.2023
  * Name: Aliyor
  */
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+
+using namespace std;
+
+vector<int> findTopKFrequent(vector<int>& nums, int k) {
+    unordered_map<int, int> frequencyMap;
+    
+    for (int num : nums) {
+        frequencyMap[num]++;
+    }
+
+    priority_queue<pair<int, int>> maxHeap;
+
+    for (auto& entry : frequencyMap) {
+        maxHeap.push({entry.second, entry.first});
+
+        if (maxHeap.size() > k) {
+            maxHeap.pop();
+        }
+    }
+
+    vector<int> result;
+    while (!maxHeap.empty()) {
+        result.push_back(maxHeap.top().second);
+        maxHeap.pop();
+    }
+
+    return result;
+}
+
+int main() {
+    int size, k;
+    cin >> size;
+
+    vector<int> nums(size);
+    for (int i = 0; i < size; ++i) {
+        cin >> nums[i];
+    }
+
+    cin >> k;
+
+    vector<int> result = findTopKFrequent(nums, k);
+
+    for (int num : result) {
+        cout << num << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
