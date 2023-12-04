331f95055ec86fe001e27c44468bcde6a93ab67f
Aliyor01 <127713784+Aliyor01@users.noreply.github.com>
2023-12-04 22:20:48 +0500

    Update task_8.h

--- src/task_8.h
+++ src/task_8.h
@@ -3,3 +3,38 @@
  * Date: 04.12.2023
  * Name: Aliyor
  */
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int determineMissingNumber(vector<int>& nums) {
+    int n = nums.size() + 1; 
+    
+    int expectedSum = (n * (n + 1)) / 2;
+
+    int actualSum = 0;
+    for (int num : nums) {
+        actualSum += num;
+    }
+
+    int missingNumber = expectedSum - actualSum;
+
+    return missingNumber;
+}
+
+int main() {
+    int size;
+    cin >> size;
+
+    vector<int> nums(size - 1);
+    for (int i = 0; i < size - 1; ++i) {
+        cin >> nums[i];
+    }
+
+    int missingNumber = determineMissingNumber(nums);
+
+    cout << "0 " << missingNumber << endl;
+
+    return 0;
+}
