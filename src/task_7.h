04ab69fb7c40a46cdca4cdf30c30b01619df58cb
Aliyor01 <127713784+Aliyor01@users.noreply.github.com>
2023-12-04 22:20:04 +0500

    Update task_7.h

--- src/task_7.h
+++ src/task_7.h
@@ -3,3 +3,25 @@
  * Date: 04.12.2023
  * Name: Aliyor
  */
+#include <iostream>
+using namespace std;
+
+int calculatePower(int base, int exponent) {
+    if (exponent == 0) {
+        return 1;
+    }
+
+    int result = base * calculatePower(base, exponent - 1);
+    return result;
+}
+
+int main() {
+    int base, exponent;
+    cin >> base;
+    cin >> exponent;
+
+    int result = calculatePower(base, exponent);
+    cout << result << endl;
+
+    return 0;
+}
