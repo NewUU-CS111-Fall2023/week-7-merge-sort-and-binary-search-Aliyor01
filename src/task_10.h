9328d8076267c67c9bbedf12416e52ff5c99afe3
Aliyor01 <127713784+Aliyor01@users.noreply.github.com>
2023-12-04 22:22:01 +0500

    Update task_10.h

--- src/task_10.h
+++ src/task_10.h
@@ -3,3 +3,34 @@
  * Date: 04.12.2023
  * Name: Aliyor
  */
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int findKthFactor(int n, int k) {
+    vector<int> factors;
+
+    for (int i = 1; i <= n; ++i) {
+        if (n % i == 0) {
+            factors.push_back(i);
+        }
+    }
+
+    if (k <= factors.size()) {
+        return factors[k - 1];
+    } else {
+        return -1;
+    }
+}
+
+int main() {
+    int number, k;
+    cin >> number >> k;
+
+    int result = findKthFactor(number, k);
+
+    cout << result << endl;
+
+    return 0;
+}
