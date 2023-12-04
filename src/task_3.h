0771911e0c952e45cb093797245b4af3ca643c1c
Aliyor01 <127713784+Aliyor01@users.noreply.github.com>
2023-12-04 22:16:56 +0500

    Update task_3.h

--- src/task_3.h
+++ src/task_3.h
@@ -3,3 +3,53 @@
  * Date: 04.12.2023
  * Name: Aliyor
  */
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int numTestCases;
+    cin >> numTestCases;
+
+    while (numTestCases--) {
+        int sizeA, sizeB;
+        cin >> sizeA >> sizeB;
+
+        vector<int> arrayA(sizeA), arrayB(sizeB), mergedResult(sizeA + sizeB);
+
+        for (int i = 0; i < sizeA; ++i) {
+            cin >> arrayA[i];
+        }
+
+        for (int i = 0; i < sizeB; ++i) {
+            cin >> arrayB[i];
+        }
+
+        int i = 0, j = 0, k = 0;
+
+        while (i < sizeA && j < sizeB) {
+            if (arrayA[i] >= arrayB[j]) {
+                mergedResult[k++] = arrayA[i++];
+            } else {
+                mergedResult[k++] = arrayB[j++];
+            }
+        }
+
+        while (i < sizeA) {
+            mergedResult[k++] = arrayA[i++];
+        }
+
+        while (j < sizeB) {
+            mergedResult[k++] = arrayB[j++];
+        }
+
+        for (int idx = 0; idx < sizeA + sizeB; ++idx) {
+            cout << mergedResult[idx] << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
